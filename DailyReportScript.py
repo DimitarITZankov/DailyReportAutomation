82e1a2c31c40444a20bafc498a5d0614587d2e03
dimitar <dimitarzankovit@gmail.com>
2025-09-22 16:37:52 +0300

    fixing some errors on my code and typos

--- DailyReportScript.py
+++ DailyReportScript.py
@@ -12,28 +12,29 @@ os.makedirs(logs_folder, exist_ok=True)
 logging.basicConfig(
     filename=os.path.join(logs_folder, "daily_report.log"),
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.INFO)
 try:
-    df = pd.read_csv("daily_sales.csv")
+    sales_df = pd.read_csv(os.path.join(data_folder, "daily_sales.csv"))
     logging.debug("Opened the  csv file successfully")
 except FileNotFoundError as e:
-    logging.error("Unable to open the csv file",e)
+    logging.error("Unable to open the csv file: %s",e)
     print("The file doesnt exist")
     quit()
 logging.debug("Fetching the data for daily report")
-calculate_kg_per_day = df.groupby("Date")["Kg"].sum()
-calculate_price_per_day = df.groupby("Date")["Price"].sum()
-all_kg = df["Kg"].sum()
+calculate_kg_per_day = sales_df.groupby("Date")["Kg"].sum()
+calculate_price_per_day = sales_df.groupby("Date")["Price"].sum()
+all_kg = sales_df["Kg"].sum()
 today = datetime.today().strftime("%Y-%m-%d")
-calculate_kg_per_day.to_csv(f"DailyReport_{today}.csv")
+calculate_kg_per_day.to_csv(os.path.join(reports_folder, f"DailyReport_{today}.csv"), index=False)
 logging.info("Daily report has been successfully generated")
 logging.info(f"Searching for file...")
 try:
-    df = pd.read_csv(f"StorageLeft.csv")
+    storage_df = pd.read_csv(os.path.join(data_folder, "StorageLeft.csv"))
     logging.debug("Opened the csv file successfully")
 except FileNotFoundError as e:
     logging.error("Unable to open the csv file: %s",e)
     print("The file doesnt exist")
-df["Main_Storage"] = df["Main_Storage"] - all_kg
-df.to_csv(f"StorageLeft.csv",index=False)
+    quit()
+storage_df["Main_Storage"] = storage_df["Main_Storage"] - all_kg
+storage_df.to_csv(os.path.join(data_folder, "StorageLeft.csv"), index=False)
 logging.info("StorageLeft has been successfully refreshed with the new data")
