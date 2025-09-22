7ee6ddd675ff2b9c65cf17cf55b63064e0b5809d
dimitar <dimitarzankovit@gmail.com>
2025-09-22 15:42:25 +0300

    Opens and reads the file

--- DailyReportScript.py
+++ DailyReportScript.py
@@ -3,3 +3,9 @@ import csv
 import pandas as pd
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+try:
+    df = pd.read_csv("daily_sales.csv")
+    logging.debug("Opened the  csv file successfully")
+except FileNotFoundError as e:
+    logging.error("Unable to open the csv file",e)
+    print("The file doesnt exist")
