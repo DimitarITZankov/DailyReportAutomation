ac417a9c12b48e8991205c0a91246adb79f8b627
dimitar <dimitarzankovit@gmail.com>
2025-09-27 15:22:36 +0300

    make somme changes about the logging module in the setting stage

--- scripts/UpgradedVersionOfDailyReportScript.py
+++ scripts/UpgradedVersionOfDailyReportScript.py
@@ -9,7 +9,7 @@ logs_folder = "../logs"
 reports_folder = "../reports"
 
 os.makedirs(logs_folder, exist_ok=True)
-logging.basicConfig(filename=os.path.join(logs_folder, "upgrade_daily_report.log"),
+logging.basicConfig(filename=os.path.join(logs_folder, "daily_report.log"),
                     format='%(asctime)s %(levelname)s %(message)s',level=logging.INFO)
 
 def open_csv(csv_file):
