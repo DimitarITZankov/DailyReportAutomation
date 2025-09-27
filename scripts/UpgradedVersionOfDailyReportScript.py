664f3e38982fc9f66bedcc966b1945e4a7a0e057
dimitar <dimitarzankovit@gmail.com>
2025-09-27 15:28:36 +0300

    creating the main function

--- scripts/UpgradedVersionOfDailyReportScript.py
+++ scripts/UpgradedVersionOfDailyReportScript.py
@@ -40,3 +40,23 @@ def save_csv(df,folder,filename):
 def upgrade_storage(df_storage,total_kg):
     df_storage["Main_Storage"] = df_storage["Main_Storage"] - total_kg
     return df_storage
+
+def main():
+    logging.info("Opening the csv file")
+    sales_file = os.path.join(data_folder, "daily_sales.csv")
+    sales_df = open_csv(sales_file)
+
+    daily_report, total_kg = generate_daily_reports(sales_df)
+    today = datetime.today().strftime("%Y-%m-%d")
+    save_csv(daily_report,reports_folder,f"daily_report.{today}")
+    logging.info("Successfully generated daily report")
+
+    storage_file = os.path.join(data_folder, "StorageLeft.csv")
+    df_storage = open_csv(storage_file)
+    df_storage = upgrade_storage(df_storage,total_kg)
+    save_csv(df_storage,data_folder,f"StorageLeft.csv")
+    logging.info("StorageLeft has been successfully refreshed with the new data")
+
+if __name__ == "__main__":
+    main()
+
