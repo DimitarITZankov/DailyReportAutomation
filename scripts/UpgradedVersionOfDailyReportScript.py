9335cda360f979542e188985a4e96f087030d10e
dimitar <dimitarzankovit@gmail.com>
2025-09-27 15:18:50 +0300

    creating the open_csv function

--- scripts/UpgradedVersionOfDailyReportScript.py
+++ scripts/UpgradedVersionOfDailyReportScript.py
@@ -12,3 +12,13 @@ os.makedirs(logs_folder, exist_ok=True)
 logging.basicConfig(filename=os.path.join(logs_folder, "upgrade_daily_report.log"),
                     format='%(asctime)s %(levelname)s %(message)s',level=logging.INFO)
 
+def open_csv(csv_file):
+    try:
+        df_sales = pd.read_csv(csv_file)
+        logging.info("Opened the csv file successfully")
+        return df_sales
+    except FileNotFoundError as e:
+        logging.error(e)
+        print("File not found")
+        quit()
+
