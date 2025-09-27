f6e871ea1647f00951402c7ef1e5f2b1264a14de
dimitar <dimitarzankovit@gmail.com>
2025-09-27 15:42:21 +0300

    creating the first test for the open_csv function

--- TestFiles/testing_the_script.py
+++ TestFiles/testing_the_script.py
@@ -1,1 +1,6 @@
 import pytest
+from scripts.UpgradedVersionOfDailyReportScript import *
+def test_open_csv_not_found(path):
+    fake_path = path / "not_existing_file.csv"
+    with pytest.raises(FileNotFoundError):
+        open_csv(fake_path)
