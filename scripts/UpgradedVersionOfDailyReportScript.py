47f4ce086a19414452ac26e115aa86bdb5c92d3d
dimitar <dimitarzankovit@gmail.com>
2025-09-27 15:17:42 +0300

    setting up the logging module

--- scripts/UpgradedVersionOfDailyReportScript.py
+++ scripts/UpgradedVersionOfDailyReportScript.py
@@ -7,3 +7,8 @@ from datetime import datetime
 data_folder= "../data"
 logs_folder = "../logs"
 reports_folder = "../reports"
+
+os.makedirs(logs_folder, exist_ok=True)
+logging.basicConfig(filename=os.path.join(logs_folder, "upgrade_daily_report.log"),
+                    format='%(asctime)s %(levelname)s %(message)s',level=logging.INFO)
+
